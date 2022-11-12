837391e21862ad8da48de4441782708e5ddc4bae
RealChiefChen <104585655+RealChiefChen@users.noreply.github.com>
2022-11-12 12:15:19 -0600

    Add files via upload

---
+++ install_dependencies.sh
@@ -0,0 +1,10 @@
+chmod +x todo
+chmod +x gui
+pip install notify2
+pip install customtkinter
+sudo apt install python3-pil python3-pil.imagetk
+sudo pacman -S python-pillow
+echo
+echo =====================================================================
+echo PLEASE INSTALL ESPEAK-NG HERE: https://github.com/espeak-ng/espeak-ng
+echo =====================================================================
