fe99805393f15970554671a0f7a00b0ed6a406c1
RealChiefChen <104585655+RealChiefChen@users.noreply.github.com>
2022-11-16 22:39:07 -0600

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,17 +8,17 @@ sudo pacman -S python-pillow
 sudo sudo mv todo /usr/local/bin
 sudo sudo mv todo-gui /usr/local/bin
 sudo mkdir /home/$USER/.config/todo
-sudo mv GaRI_bruh.png /home/chiefchen/.config/todo/GaRI_bruh.png
-sudo mv GaRI_happy.png /home/chiefchen/.config/todo/GaRI_happy.png
-sudo mv GaRI_really_happy.png /home/chiefchen/.config/todo/GaRI_really_happy.png
-sudo mv GaRI_really_sad.png /home/chiefchen/.config/todo/GaRI_really_sad.png
-sudo mv GaRI_sad.png /home/chiefchen/.config/todo/GaRI_sad.png
-sudo mv todo_GaRI.png /home/chiefchen/.config/todo/todo_GaRI.png
-sudo mv config.ini /home/chiefchen/.config/todo/config.ini
-sudo mv install_dependencies.sh /home/chiefchen/.config/todo/install_dependencies.sh
-sudo mv LICENSE /home/chiefchen/.config/todo/LICENSE
-sudo mv README.md /home/chiefchen/.config/todo/README.md
-sudo mv reminder.txt /home/chiefchen/.config/todo/reminder.txt
+sudo mv GaRI_bruh.png /home/$USER/.config/todo/GaRI_bruh.png
+sudo mv GaRI_happy.png /home/$USER/.config/todo/GaRI_happy.png
+sudo mv GaRI_really_happy.png /home/$USER/.config/todo/GaRI_really_happy.png
+sudo mv GaRI_really_sad.png /home/$USER/.config/todo/GaRI_really_sad.png
+sudo mv GaRI_sad.png /home/$USER/.config/todo/GaRI_sad.png
+sudo mv todo_GaRI.png /home/$USER/.config/todo/todo_GaRI.png
+sudo mv config.ini /home/$USER/.config/todo/config.ini
+sudo mv install_dependencies.sh /home/$USER/.config/todo/install_dependencies.sh
+sudo mv LICENSE /home/$USER/.config/todo/LICENSE
+sudo mv README.md /home/$USER/.config/todo/README.md
+sudo mv reminder.txt /home/$USER/.config/todo/reminder.txt
 echo
 echo =====================================================================
 echo PLEASE INSTALL ESPEAK-NG HERE: https://github.com/espeak-ng/espeak-ng
